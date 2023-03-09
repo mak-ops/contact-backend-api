50c153c69038dd56cf88bc74fd8bb46e551e6b8c
akhlaque <mdakhlaque993947@gmail.com>
2023-03-09 20:06:00 +0530

    fix: conection with mongoose

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorhandler");
 const dotenv = require("dotenv").config();
 const app= express();
 const port =process.env.PORT || 5000;
-
+connectDb();
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
